ae2bd562ca53c29aa1098f010625da8ce8ea273a
Vamsidhar <manaswini.01996@gmail.com>
2024-01-28 16:48:21 -0600

    posts api

---
+++ posts/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+ const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(6).toString('hex');
+    const { title } = req.body;
+
+    // return 400 if no title
+    if (!title) {
+        return res.status(400).send('Title is required');
+    }
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+
+app.listen(4000, () => {
+    console.log('Listening on 4000');
+});
